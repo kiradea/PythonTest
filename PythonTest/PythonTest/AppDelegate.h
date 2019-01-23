08d9d22fa202e28729cf035aec28545e99ef4a63
edz <jun.li@enwaysoft.com>
2019-01-23 17:09:20 +0800

    初始化

---
+++ PythonTest/PythonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PythonTest
+//
+//  Created by edz on 2019/1/21.
+//  Copyright © 2019 TestDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PythonTest/PythonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PythonTest
+//
+//  Created by edz on 2019/1/21.
+//  Copyright © 2019 TestDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
